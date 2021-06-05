5dbd29111759dee8e6b8959df3a300814dc0fe52
CarlosLoeza <loeza.carlos@yahoo.com>
2021-06-04 18:58:04 -0700

    Working on displaying the score using the LED matrix

--- source/main.c
+++ source/main.c
@@ -1,21 +1,25 @@
-/*    Author: lab
- *  Partner(s) Name:
- *    Lab Section:
- *    Assignment: Lab #  Exercise #
- *    Exercise Description: [optional - include for your own benefit]
+
+/*      Author: lab
+ *  Partner(s) Name: 
+ *      Lab Section:
+ *      Assignment: Lab #  Exercise #
+ *      Exercise Description: [optional - include for your own benefit]
  *
- *    I acknowledge all content contained herein, excluding template or example
- *    code, is my own original work.
+ *      I acknowledge all content contained herein, excluding template or examp$
+ *      code, is my own original work.
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
 volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programme$
+
 // Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
 unsigned long _avr_timer_M = 1; // Start counter from here to 0. Default 1ms
 unsigned long _avr_timer_cntcurr = 0; // Current internal clock of 1ms ticks
+
 void TimerOn(){
     // AVR timer/counter controller register TCCR1
     TCCR1B = 0x0B;      // bit3 = 0: CTC mode(clear timer on compare)
@@ -23,24 +27,32 @@ void TimerOn(){
                         // 0000 1011 : 0x0B
                         // So 8 MHz clock or 8,000,000 / 64  = 125,000 ticks
                         // Thus TCNT1 register will count 125,000 ticks
+
     // AVR out compare register OCR1A
     OCR1A = 125;        // Timer interrupt will be generated when TCNT1==OCR1A
                         // We want a 1ms tick. .001s * 125,000 ticks = 125
                         // So when TCNT1 == 125 then that means 1ms has passed
+
     // AVR timer register interrupt mask register
     TIMSK1 = 0x02;      // bit1: OCIE1A -- enables compare match register
+
     // Initiliaze AVR counter
     TCNT1 = 0;
+
     _avr_timer_cntcurr = _avr_timer_M;
+
     // enable global interrupts
-    SREG |= 0x80;       // 1000 0000
+    SREG |= 0x80;       // 1000 0000 
 }
+
 void TimerOff(){
     TCCR1B = 0x00;
 }
+
 void TimerISR(){
     TimerFlag = 1;
 }
+
 // In our approach C program does not touch this ISR, only TimerISR()
 ISR(TIMER1_COMPA_vect){
     // CPU automatically calls when TCNT1 == OCR1 (Every 1ms per TimerOn settin$
@@ -50,205 +62,78 @@ ISR(TIMER1_COMPA_vect){
         _avr_timer_cntcurr = _avr_timer_M;
     }
 }
+
 // Set timer to tick every M ms.
 void TimerSet(unsigned long M){
     _avr_timer_M = M;
     _avr_timer_cntcurr =  _avr_timer_M;
 }
 
-unsigned char button;
-unsigned char btn;
-unsigned short x;
-int i = 0;
-unsigned char falling_pattern;
-unsigned char falling_row;
 
-enum Falling_States {shift};
-int Falling_Object(int state) {
-    // Local Variables
-    static unsigned long patterns[] = {0x02, 0x80, 0x01, 0x10};        // LED pattern - 0: LED off; 1: LED on
-    static unsigned char row = 0xFE;    // Row(s) displaying pattern.
-                                                        // 0: display pattern on row   $
-    // Transitions
-    switch (state) {
-        case shift:
+
+
+
+enum Hello_States{Hello_Start, Hello_1, Hello_2, Hello_3} Hello_State;
+void Hello(){ 
+    switch(Hello_State){
+	case Hello_Start:
+	    Hello_State = Hello_1;
+	    break;
+	case Hello_1:
+	    Hello_State = Hello_2;
             break;
-        default:
-            state = shift;
-            break;
+	case Hello_2:
+	    Hello_State = Hello_3;
+	    break;
+	case Hello_3:
+	    Hello_State = Hello_1;
+	    break;
+	default:
+	    Hello_State = Hello_Start;
+	    break;
     }
-    // Actions
-    switch (state) {
-        case shift:
-        // if row != last row, go to next row
-            if (row != 0xEF){
-                row = ~row;
-                row = row << 1;
-                row = ~row;
-            } else {
-                row = 0xFE;
-              if(i<3){ i++; } else {i = 0;}
-        }
+
+    switch(Hello_State){
+	case Hello_1:
+	    PORTC = 0x10;
+	    PORTD = 0x08;
+	    break;
+	case Hello_2:
+            PORTC = 0x04;
+	    PORTD = 0x02;
             break;
-        default:
-            break;
+	case Hello_3:
+	    PORTC = 0x08;
+	    PORTD = 0x0A;
+	    break;
+
     }
-    
-    
-    
-    falling_pattern = patterns[i];    // Pattern to display
-    falling_row = row;                // Row(s) displaying pattern
-    return state;
 }
-
-
-unsigned char set_row;
-unsigned char set_pattern;
-unsigned char joystick_pattern;
-unsigned char joystick_row;
-//unsigned char set_row;
-enum Joystick_States {Joystick_Start, Joystick_Wait, Joystick_Right, Joystick_Left} Joystick_State;
-//unsigned char set_pattern = 0xE0; // joystick pattern
-void Joystick(){
-    static unsigned char row = 0xEF;
-    //x = ADC;
-    // Transition
-    switch(Joystick_State){
-        case Joystick_Start:
-            Joystick_State = Joystick_Wait;
-            button = 0;
-            break;
-        case Joystick_Wait:
-            if(x > 512 && x < 600){
-                button = 0;
-                Joystick_State = Joystick_Wait;
-            }
- 
-        else if(x > 600){
-                Joystick_State = Joystick_Right;
-                button = 3;
-            } else if(x < 512){
-                Joystick_State = Joystick_Left;
-                button = 4;
-            }
-            break;
-    // if left or right
-        case Joystick_Right:
-        Joystick_State = Joystick_Wait;
-        break;
-        case Joystick_Left:
-            Joystick_State = Joystick_Wait;
-            break;
-        default:
-            Joystick_State = Joystick_Wait;
-            break;
-    }
-    // Action
-    switch(Joystick_State){
-        // Shift column to the right
-        case Joystick_Right:
-            if(set_pattern == 0x07 && button == 3)
-            set_pattern = 0x07;
-            else if(set_pattern != 0x07 && button == 3)
-                set_pattern = (set_pattern >> 1);
-            break;
-        // Shift column to the left
-        case Joystick_Left:
-            if (set_pattern == 0xE0 && button == 4)
-            set_pattern = 0xE0;
-        else if(set_pattern != 0xE0 && button == 4)
-                set_pattern = (set_pattern << 1);
-            break;
-        default:
-            break;
-    }
-    joystick_pattern = set_pattern;
-    joystick_row = set_row;
-//    PORTC = set_pattern;
-//    PORTD = row;
-}
-
-
-void A2D_init() {
-    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
-}
-   
-
-enum LED_States{LED_Start, LED_Falling_Object, LED_Joystick} LED_State;
-void LED_Display(){
-
-    switch(LED_State){
-        case LED_Start:
-            LED_State = LED_Falling_Object;
-            break;
-        case LED_Falling_Object:
-            LED_State = LED_Joystick;
-            break;
-        case LED_Joystick:
-            LED_State = LED_Falling_Object;
-            break;
-        default:
-            LED_State = LED_Start;
-            break;
-        }
-
-    switch(LED_State){
-        case LED_Falling_Object:
-            PORTC = falling_pattern;
-            PORTD = falling_row;
-            break;
-        case LED_Joystick:
-            PORTC = joystick_pattern;
-            PORTD = joystick_row;
-            break;
-    }
-    
-}
-    
-
-
 
 
 int main(void) {
-    DDRA = 0x00; PORTA = 0x0F;
+    /* Insert DDR and PORT initializations */
+    
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
-    
-    set_row = 0xEF;
-    set_pattern = 0xE0;
+    DDRA = 0x00; PORTA = 0x0F;
 
-    unsigned long timer = 10;
-    unsigned long Falling_Object_timer = 400;
 
-    Joystick_State = Joystick_Start;
-    unsigned long Joystick_timer = 100;
-
-    //static unsigned short x;
-    LED_State = LED_Start;
-  
-    int state = 0;
-    TimerSet(timer);
+    TimerSet(1);
     TimerOn();
-    A2D_init();
     /* Insert your solution below */
     while (1) {
-        x = ADC;
-        if(Falling_Object_timer >= 400){
-            state = Falling_Object(state);
-            Falling_Object_timer = 0;
-        }
-        if(Joystick_timer >= 100){
-            Joystick();
-            Joystick_timer = 0;
-        }
-        
-        LED_Display();
-        
-        while(!TimerFlag);
+	//btn = ~PINA & 0x0F;
+        //Calculate();
+	//state = Demo_Tick(state);
+	
+	Hello();	
+	while(!TimerFlag);
         TimerFlag = 0;
-        Falling_Object_timer += timer;
-        Joystick_timer += timer;
     }
-
+    return 1;
 }
+
+
+
 
